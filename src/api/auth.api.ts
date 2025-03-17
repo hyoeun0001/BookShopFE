d5d03a156eae7cc11c9685286c9fe226e68a0aea
hyoeun0001 <83484611+hyoeun0001@users.noreply.github.com>
2025-03-17 22:31:37 +0900

    로그아웃 수정 및 props 분리

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,5 +1,7 @@
+import { ResetPasswordProps } from "@/pages/ResetPassword";
 import { SignupProps } from "../pages/Signup";
 import { httpClient } from "./http";
+import { LoginProps } from "@/pages/Login";
 
 export const signup = async (userData: SignupProps) => {
   const response = await httpClient.post("/api/members/join", userData);
@@ -7,13 +9,13 @@ export const signup = async (userData: SignupProps) => {
   return response.data;
 };
 
-export const resetRequest = async (data: SignupProps) => {
+export const resetRequest = async (data: ResetPasswordProps) => {
   const response = await httpClient.post("/api/members/reset", data);
 
   return response.data;
 };
 
-export const resetPassword = async (data: SignupProps) => {
+export const resetPassword = async (data: ResetPasswordProps) => {
   const response = await httpClient.put("/api/members/reset", data);
 
   return response.data;
@@ -23,14 +25,19 @@ interface LoginResponse {
   token: string;
 }
 
-export const login = async (data: SignupProps) => {
+export const login = async (data: LoginProps) => {
   const response = await httpClient.post<LoginResponse>("/api/members/login", data);
 
   return response.data;
 };
 
 export const logout = async () => {
-  await httpClient.post("/api/members/logout", {}, { withCredentials: true });
+  try {
+    await httpClient.post("/api/members/logout", {}, { withCredentials: true });
 
-  document.cookie = "jwtToken=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+    // 새로고침하여 상태 반영
+    window.location.href = "/";
+  } catch (error) {
+    console.error("로그아웃 실패:", error);
+  }
 };
